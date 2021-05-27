018e889b015aeb55165329749212dc0e0afd39ce
Imran Ishaq <imranishaq7071@gmail.com>
2021-05-28 02:59:31 +0500

    get wit param

--- src/main/java/com/imranishaqcode/docker/demo/controller/DemoController.java
+++ src/main/java/com/imranishaqcode/docker/demo/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.imranishaqcode.docker.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,12 @@ public class DemoController {
     @GetMapping(path = "/get")
     public String Hello(){
         return "Docker File Image Demo";
+    }
+
+
+
+    @GetMapping(path = "/getName")
+    public String Hello(@RequestParam String name){
+        return "Hello"+name;
     }
 }
